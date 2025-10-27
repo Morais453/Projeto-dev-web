4e5cf7e5fff0943cd03bc28f1d64f76af81134e2
Morais453 <danmorais453@hotmail.com>
2025-10-27 16:47:30 -0300

    feat(backend): setup Spring Boot structure with Lombok, Spring Web, JPA, and PostgreSQL Driver dependencies

---
+++ Backend/GerenciamentoEsportivo/src/main/java/com/edu/ufra/GerenciamentoEsportivo/GerenciamentoEsportivoApplication.java
@@ -0,0 +1,13 @@
+package com.edu.ufra.GerenciamentoEsportivo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciamentoEsportivoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciamentoEsportivoApplication.class, args);
+	}
+
+}
---
+++ Backend/GerenciamentoEsportivo/src/test/java/com/edu/ufra/GerenciamentoEsportivo/GerenciamentoEsportivoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edu.ufra.GerenciamentoEsportivo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciamentoEsportivoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
